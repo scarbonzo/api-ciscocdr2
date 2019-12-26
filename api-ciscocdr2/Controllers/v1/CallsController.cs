4cd26a6bebb5ff230cfa6294fa0ae1b2179a3137
Rich Eodice <rich@eodice.com>
2019-12-26 15:17:31 -0500

    Initial build, hooked into DB and created a basic read-only controller for the Calls.cs class.

---
+++ api-ciscocdr2/Controllers/v1/CallsController.cs
@@ -0,0 +1,87 @@
+﻿using api_ciscocdr2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace api_ciscocdr2.Controllers.v1
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("v1/calls")]
+    public class CallsController : ControllerBase
+    {
+        private readonly CallAnalyzerContext _context;
+
+        public CallsController(CallAnalyzerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get(DateTime? Start = null, DateTime? End = null, string Number = null, string Device = null, string Cause = null, int take = 25, int skip = 0)
+        {
+            if (Start == null)
+            {
+                Start = DateTime.Now.Date;
+            }
+
+            if (End == null)
+            {
+                End = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+            }
+
+            var calls = _context.Calls
+                .Where(c => c.DateTimeDisconnect >= Start && c.DateTimeDisconnect <= End);
+
+            if (Number != null)
+            {
+                calls = calls
+                    .Where(c => c.CallingPartyNumber.Contains(Number) || c.OriginalCalledPartyNumber.Contains(Number) || c.FinalCalledPartyNumber.Contains(Number));
+            }
+
+            if (Device != null)
+            {
+                calls = calls
+                    .Where(c => c.OrigDeviceName.Contains(Device) || c.DestDeviceName.Contains(Device));
+            }
+
+            if (Cause != null)
+            {
+                calls = calls
+                    .Where(c => c.OrigCauseValue == Cause || c.DestCauseValue == Cause);
+            }
+
+            var results = calls
+                .Skip(skip)
+                .Take(take)
+                .OrderByDescending(c => c.DateTimeDisconnect);
+
+            return Ok(results);
+        }
+
+        [HttpGet]
+        [Route("{Id}")]
+        public ActionResult GetById(string Id)
+        {
+            try
+            {
+                var result = _context.Calls
+                    .Where(c => c.Pkid == "\"" + Id + "\"")
+                    .FirstOrDefault();
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+                catch
+            {
+                return BadRequest(Id);
+            }
+        }
+    }
+}
---
+++ api-ciscocdr2/Startup.cs
@@ -0,0 +1,51 @@
+using api_ciscocdr2.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace api_ciscocdr2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Database contexts
+            services.AddDbContext<CallAnalyzerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CallAnalyzerDatabase")));
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
